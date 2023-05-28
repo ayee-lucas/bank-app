18114239c00f378fcb3850b471d6a263b78903a8
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-28 12:16:09 -0600

    nextauth setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,53 @@
+import NextAuth from "next-auth/next";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+const nextUrl = process.env.NEXTAUTH_URL as string;
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch(`http://localhost:3000/api/login/`, {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ app/api/login/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from "next/server";
+import User, {IUser} from "@/app/models/User"
+import { comparePassword } from "@/app/tools/encrypt";
+import { JWT } from "@/app/tools/jwt";
+import dbConnect from "@/app/db/connection";
+
+dbConnect();
+
+export async function POST(req: NextRequest) {
+  try {
+    const json: IUser = await req.json();
+
+    const credentials = {
+      username: json.username,
+      password: json.password,
+    };
+
+    if (credentials.username.length < 3) {
+      return new NextResponse(
+        JSON.stringify({ message: "Username too short" }),
+        { status: 400 }
+      );
+    }
+
+    if (credentials.password.length < 8) {
+      return new NextResponse(
+        JSON.stringify({ message: "Password too short" }),
+        { status: 400 }
+      );
+    }
+
+    const user = await User.findOne({ username: json.username });
+
+    if (!user)
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+
+    const isPasswordValid = await comparePassword(
+      credentials.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return new NextResponse(JSON.stringify({ message: "Invalid password" }), {
+        status: 401,
+      });
+    }
+
+    const token = await JWT(user);
+
+    const logged = {
+      id: user.id,
+      sub: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      accessToken: token,
+    };
+
+    return new NextResponse(JSON.stringify(logged), {
+      status: 200,
+    });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(JSON.stringify(err), { status: 500 });
+  }
+}

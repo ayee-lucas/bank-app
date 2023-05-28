045864dcee8db7f4b2f95ffdbe4e5bf4f5446712
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-05-28 12:17:12 -0600

    Added nextauth session

---
+++ app/types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth/next";
+
+declare module "next-auth" {
+    interface Session {
+        user:{
+            id: number;
+            sub:  string;
+            username: string;
+            email: string; 
+            role: string
+            accessToken: string;
+            image: string;
+            picture:string;
+        }
+    }
+}

443be152c00ef0c334532bd37bf0a2067e416426
pvasquez-2018044 <pvasquez-2018044@kinal.edu.gt>
2023-06-22 09:30:52 -0600

    Deposit get added

--- app/api/deposit/route.ts
+++ app/api/deposit/route.ts
@@ -34,3 +34,30 @@ export async function POST(request: NextRequest) {
     return new NextResponse(JSON.stringify(error), { status: 500 });
   }
 }
+
+export async function GET(request: NextRequest) {
+  try {
+    //const session = await getServerSession(authOptions);
+    // Verify if the user is authenticated and is an admin
+    /*if (!session?.user || session.user.role !== "admin") {
+      return new NextResponse("Unauthorized", {
+        status: 401,
+      });
+    }*/
+
+    // Get all account types with related data
+    const deposit = await Deposit.find();
+    const data = deposit;
+
+    if (deposit.length === 0) {
+      return new NextResponse(JSON.stringify({ message: "No Deposits Yet" }), {
+        status: 200,
+      });
+    }
+
+    return new NextResponse(JSON.stringify(data), { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return new NextResponse(JSON.stringify(err), { status: 500 });
+  }
+}

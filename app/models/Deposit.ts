afc54d8904297bae8311f12304501673d5860f30
pvasquez-2018044 <pvasquez-2018044@kinal.edu.gt>
2023-06-22 12:53:56 -0600

    One minute validation and search by id

--- app/models/Deposit.ts
+++ app/models/Deposit.ts
@@ -14,6 +14,12 @@ const DepositSchema = new Schema<IDeposit>(
     amount: {
       type: Number,
       required: true,
+      validate: {
+        validator: function (value: number) {
+          return value <= 10000;
+        },
+        message: "El monto máximo permitido es de 10000.",
+      },
     },
     account: {
       type: String,
@@ -53,6 +59,26 @@ DepositSchema.pre<IDeposit>("save", async function (next: Function) {
     return next();
   } catch (error: any) {
     return next(error);
+  }
+});
+
+DepositSchema.post("findOneAndDelete", async function (doc: IDeposit) {
+  try {
+    const accountNumber = doc.account;
+    const amount = doc.amount;
+
+    // Fetch the account
+    const account = await BankAccount.findOne({ accNumber: accountNumber });
+
+    // Revert the deposit amount from the account balance
+    if (account) {
+      account.balance -= amount;
+      await account.save();
+    } else {
+      throw new Error("Account not found");
+    }
+  } catch (error: any) {
+    console.log(error);
   }
 });
 

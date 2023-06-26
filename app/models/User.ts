c8a8437bf3d89babae7cb1d1e40711f8b1b35c8c
pvasquez-2018044 <108350937+pvasquez-2018044@users.noreply.github.com>
2023-06-26 14:34:31 +0000

    Required added in User model

--- app/models/User.ts
+++ app/models/User.ts
@@ -23,6 +23,7 @@ const userSchema = new Schema<IUser>(
   {
     name: {
       type: String,
+      required: true,
     },
     username: {
       type: String,
@@ -47,20 +48,25 @@ const userSchema = new Schema<IUser>(
     },
     dpi: {
       type: String,
+      required: true,
       minlength: 13,
       maxlength: 13,
     },
     address: {
       type: String,
+      required: true,
     },
     phone: {
       type: String,
+      required: true,
     },
     work: {
       type: String,
+      required: true,
     },
     salary: {
       type: String,
+      required: true,
     },
     role: {
       type: String,

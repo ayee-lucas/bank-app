b3f8bf6a7f2ce9f938cb00eae956388f87986d81
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-06-27 19:36:30 -0600

    New features in page server component

--- app/console/AccountType/page.tsx
+++ app/console/AccountType/page.tsx
@@ -1,21 +1,54 @@
 import TableView from "./components/TableView";
 import Link from "next/link";
-import { getAccountTypes } from "./action";
+import { getAccountById, getAccountTypes } from "./action";
 import { IAccountType } from "@/app/models/AccountType";
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns-tsx";
+import Delete from "./components/Delete";
+import NewModal from "./components/NewModal";
+import EditModal from "../../FormComponents/EditModal";
+import AccTypeFEdit from "./components/EditForm";
 
-export default async function AccountType() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const accounts: IAccountType[] = await getAccountTypes();
 
-  const accounts:IAccountType[] = await getAccountTypes();
+  let accountExists = null;
+
+  console.log(searchParams?.edit);
+
+  if (searchParams?.edit) {
+    accountExists = await getAccountById(searchParams?.edit);
+
+    console.log(accountExists);
+
+    if (!accountExists) {
+      return <div>Account not found</div>;
+    }
+  }
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full min-h-screen flex flex-col gap-3 p-10 overflow-y-auto">
+      <div className="text-2xl lg:text-4xl xl:text-7xl font-bold text-violet-600">
+        Account Types
+      </div>
+      <Delete />
 
-      <div className="p-6 text-3xl font-semibold text-violet-900 underline underline-offset-8">AccountType</div>
+      {searchParams?.edit && searchParams?.edit !== "" && (
+        <EditModal
+          title="Edit Account type"
+          description="This action will edit the account type and save it to the system."
+          redirectOnClose="/console/AccountType"
+          formFunction={<AccTypeFEdit defaultValues={{ ...accountExists }} />}
+        />
+      )}
 
-      <Link href="/console/AccountType/Add" className="mx-6 px-4 py-2 text-md font-semibold text-white-900 bg-violet-800 text-white rounded-xl max-w-fit hover:bg-violet-600">Add Account Type</Link>
+      {searchParams?.new && searchParams?.new == "true" && <NewModal />}
 
-      <TableView accounts={accounts}/>
-
+      <DataTable columns={columns} data={accounts} />
     </div>
   );
 }
